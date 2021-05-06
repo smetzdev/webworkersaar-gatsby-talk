e94ba34d7679d5cd8e22901c0cc3c7037d4b1372
smetzdev <metz@raum3.de>
2021-05-06 16:20:25 +0200

    Add posts to frontpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,35 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const posts = data.allWpPost.nodes
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <ul>
+        {posts.map((post, key) => (
+          <li>
+            <Link to={post.slug}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allWpPost {
+      nodes {
+        title
+        slug
+      }
+    }
+  }
+`
 
 export default IndexPage
